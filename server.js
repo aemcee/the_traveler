5ac85186e129178d08b9ff1dbed174db43d308a1
aemcee <acapili07@gmail.com>
2019-08-31 17:19:46 +0000

    Base server setup with single endpoint for testing

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// console.log('ENV:', ENV);
+
+if(ENV !== 'production'){
+    const cors = require('cors');
+
+    app.use(cors());
+
+}
+
+app.get('/api/test', (req, res) => {
+    res.send({
+        message: 'API test successful',
+        method: req.method,
+        url: req.url
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Server Running @ localhost:%s', PORT);
+}).on('error', (error) => {
+    console.log('Server listen error. Do you already have a server running on PORT:%s', PORT);
+});
