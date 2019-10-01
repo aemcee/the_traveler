8af786437d9e86f1cbb08459f630a5f474dd90d2
aemcee <43530130+aemcee@users.noreply.github.com>
2019-10-01 09:15:45 -0700

    Revert "Port comp to jsx - basic layout foundation functional"

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import '../assets/css/app.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ApiTest from './api_test';
-import Display from './display';
-import Landing from './landing';
-import Upload from './upload';
-import Nav from './nav';
 
 const App = () => (
     <div className="app">
-        <Nav/>
-        <Route path="/" exact component={Landing}/>
-        <Route path="/display" component={Display}/>
-        <Route path="/upload" component={Upload}/>
+        <h1 className="text-center">The Traveler</h1>
+        <ApiTest/>
     </div>
 );
 
